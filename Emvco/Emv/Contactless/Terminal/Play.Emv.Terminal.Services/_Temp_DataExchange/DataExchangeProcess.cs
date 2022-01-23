06ceaf41f325f812f1506da32b88588736ea73cf
play-venereal <jessevaughn.play.solutions@gmail.com>
2022-01-23 05:06:50 -0600

    asdf

---
+++ Emvco/Emv/Contactless/Terminal/Play.Emv.Terminal.Services/_Temp_DataExchange/DataExchangeProcess.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Play.Core.Threads;
+using Play.Emv.Terminal.Contracts.SignalIn;
+
+namespace Play.Emv.Terminal.Services._Temp_DataExchange
+{
+
+
+    internal class DataExchangeProcess : CommandProcessingQueue
+    {
+        #region Constructor
+
+        public DataExchangeProcess() : base(new CancellationTokenSource())
+        { }
+
+        #endregion
+
+        #region Instance Members
+
+        internal void Enqueue(QueryTerminalRequest request)
+        {
+            Enqueue((dynamic) request);
+        }
+
+        private async Task Handle(QueryTerminalRequest request)
+        {
+            throw new NotImplementedException();
+
+            //await Task.Run(() => { _TerminalStateMachine.Handle(request); }, _CancellationTokenSource.Token).ConfigureAwait(false);
+        }
+
+        protected override Task Handle(dynamic command)
+        {
+            return Handle(command);
+        }
+
+        #endregion
+    }
+}
