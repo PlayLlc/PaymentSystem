1716d5555ee1630f05c6b7ef9c01b3c34fa759e8
play-venereal <jessevaughn.play.solutions@gmail.com>
2022-01-22 21:36:48 -0600

    asdf

--- Emvco/Emv/Contactless/Kernel/Play.Emv.Kernel.Contracts/Configuration/CertificateAuthorityDataset.cs
+++ Emvco/Emv/Contactless/Kernel/Play.Emv.Kernel.Contracts/Configuration/CertificateAuthorityDataset.cs
@@ -4,6 +4,7 @@ using System.Linq;
 
 using Play.Core.Exceptions;
 using Play.Emv.DataElements;
+using Play.Emv.DataElements.CertificateAuthority;
 using Play.Icc.FileSystem.DedicatedFiles;
 
 namespace Play.Emv.Configuration;
