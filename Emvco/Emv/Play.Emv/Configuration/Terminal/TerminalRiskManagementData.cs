4b718686b3a0fe86029240e32e0a776b9c146b5c
play-venereal <jessevaughn.play.solutions@gmail.com>
2022-02-08 05:32:16 -0600

    asdf

--- Emvco/Emv/Play.Emv/Configuration/Terminal/TerminalRiskManagementData.cs
+++ Emvco/Emv/Play.Emv/Configuration/Terminal/TerminalRiskManagementData.cs
@@ -12,7 +12,7 @@ using Play.Emv.Ber.DataObjects;
 namespace Play.Emv.Configuration;
 
 /// <summary>
-///     Description: Application-specific value used by the cardholder device for risk management purposes.
+///     Description: Application-specific value used by card for risk management purposes.
 /// </summary>
 public record TerminalRiskManagementData : DataElement<ulong>, IEqualityComparer<TerminalRiskManagementData>
 {
@@ -33,41 +33,9 @@ public record TerminalRiskManagementData : DataElement<ulong>, IEqualityComparer
 
     #region Instance Members
 
-    public bool CardCapture() => _Value.IsBitSet(22);
-    public bool CdCvmBypassRequested() => _Value.IsBitSet(32);
-    public bool CdCvmContact() => _Value.IsBitSet(11);
-    public bool CdCvmContactless() => _Value.IsBitSet(3);
-    public bool CdCvmWithoutCdaSupported() => _Value.IsBitSet(22);
-    public bool CombinedDataAuthentication() => _Value.IsBitSet(20);
-    public bool CvmLimitExceeded() => _Value.IsBitSet(16);
-    public bool Dda() => _Value.IsBitSet(23);
-    public bool EmvModeContactlessTransactionsNotSupported() => _Value.IsBitSet(23);
-    public bool EncipheredPinForOfflineVerification() => _Value.IsBitSet(13);
-    public bool EncipheredPinForOnlineVerification() => _Value.IsBitSet(15);
-    public bool EncipheredPinVerificationPerformedByIccContact() => _Value.IsBitSet(13);
-    public bool EncipheredPinVerificationPerformedByIccContactless() => _Value.IsBitSet(5);
-    public bool EncipheredPinVerifiedOnlineContact() => _Value.IsBitSet(15);
-    public bool EncipheredPinVerifiedOnlineContactless() => _Value.IsBitSet(7);
     public override BerEncodingId GetBerEncodingId() => BerEncodingId;
     public override Tag GetTag() => Tag;
     public override ushort GetValueByteCount(BerCodec codec) => codec.GetByteCount(GetBerEncodingId(), _Value);
-    public bool IcWithContacts() => _Value.IsBitSet(6);
-    public bool MagneticStripe() => _Value.IsBitSet(7);
-    public bool MagstripeModeContactlessTransactionsNotSupported() => _Value.IsBitSet(24);
-    public bool ManualKeyEntry() => _Value.IsBitSet(8);
-    public bool NoCardVerificationMethodRequired() => _Value.IsBitSet(12);
-    public bool NoCardVerificationMethodRequiredContact() => _Value.IsBitSet(12);
-    public bool NoCardVerificationMethodRequiredContactless() => _Value.IsBitSet(4);
-    public bool PlaintextPinForIccVerification() => _Value.IsBitSet(16);
-    public bool PlaintextPinVerificationPerformedByIccContact() => _Value.IsBitSet(10);
-    public bool PlaintextPinVerificationPerformedByIccContactless() => _Value.IsBitSet(2);
-    public bool PresentAndHoldSupported() => _Value.IsBitSet(1);
-    public bool RestartSupported() => _Value.IsBitSet(8);
-    public bool ScaExempt() => _Value.IsBitSet(31);
-    public bool SignaturePaper() => _Value.IsBitSet(14);
-    public bool SignaturePaperContact() => _Value.IsBitSet(14);
-    public bool SignaturePaperContactless() => _Value.IsBitSet(6);
-    public bool StaticDataAuthentication() => _Value.IsBitSet(24);
 
     #endregion
 
@@ -110,6 +78,112 @@ public record TerminalRiskManagementData : DataElement<ulong>, IEqualityComparer
     }
 
     public int GetHashCode(TerminalRiskManagementData obj) => obj.GetHashCode();
+
+    #endregion
+
+    #region Byte 1
+
+    public bool PresentAndHoldSupported() => _Value.IsBitSet(1);
+
+    /// <summary>
+    ///     The terminal captures the PIN from the user and sends it in clear text to the chip card. The chip compares the
+    ///     value received with a witness value stored in its permanent memory since the personalization stage
+    /// </summary>
+    public bool PlaintextPinVerificationPerformedByIccForContactless() => _Value.IsBitSet(2);
+
+    /// <summary>
+    ///     Consumer Device Cardholder Verification Method
+    /// </summary>
+    /// <returns></returns>
+    public bool CdCvmForContactless() => _Value.IsBitSet(3);
+
+    /// <summary>
+    ///     This method consists of accepting without proof that the person at the point of service is that to whom the card
+    ///     was issued
+    /// </summary>
+    public bool NoCardVerificationMethodRequiredForContactless() => _Value.IsBitSet(4);
+
+    /// <summary>
+    ///     The terminal captures the PIN from the user and sends it encrypted in an RSA envelope to the chip card. The chip
+    ///     decrypts the envelope, retrieves the PIN in clear, and compares the retrieved value with a witness value stored in
+    ///     its permanent memory since the personalization stage.
+    /// </summary>
+    public bool EncipheredPinVerificationPerformedByIccForContactless() => _Value.IsBitSet(5);
+
+    /// <summary>
+    ///     This CVM can be applied for credit card products at a point of service that is attended by an operator. The method
+    ///     consists of comparing the signature produced by the card user on the sales slip against the witness signature of
+    ///     the cardholder written on the back side of the card
+    /// </summary>
+    public bool SignaturePaperForContactless() => _Value.IsBitSet(6);
+
+    /// <summary>
+    ///     The cardholder types his or her PIN in the terminal's PIN pad. The terminal encrypts it using a symmetric
+    ///     encryption mechanism. The IH receives this cryptogram, decrypts it in a secure module, which computes a PIN image
+    ///     control value that is compared against a witness value kept in the cardholder database, referred to as the PIN
+    ///     image stored value
+    /// </summary>
+    public bool EncipheredPinVerifiedOnlineForContactless() => _Value.IsBitSet(7);
+
+    public bool IsRestartSupported() => _Value.IsBitSet(8);
+
+    #endregion
+
+    #region Byte 2
+
+    /// <summary>
+    ///     The terminal captures the PIN from the user and sends it in clear text to the chip card. The chip compares the
+    ///     value received with a witness value stored in its permanent memory since the personalization stage
+    /// </summary>
+    public bool PlaintextPinVerificationPerformedByIccForContact() => _Value.IsBitSet(10);
+
+    public bool CdCvmForContact() => _Value.IsBitSet(11);
+
+    /// <summary>
+    ///     This method consists of accepting without proof that the person at the point of service is that to whom the card
+    ///     was issued
+    /// </summary>
+    public bool NoCardholderVerificationMethodRequiredForContact() => _Value.IsBitSet(12);
+
+    /// <summary>
+    ///     The terminal captures the PIN from the user and sends it encrypted in an RSA envelope to the chip card. The chip
+    ///     decrypts the envelope, retrieves the PIN in clear, and compares the retrieved value with a witness value stored in
+    ///     its permanent memory since the personalization stage.
+    /// </summary>
+    public bool EncipheredPinVerificationPerformedByIccForContact() => _Value.IsBitSet(13);
+
+    /// <summary>
+    ///     This CVM can be applied for credit card products at a point of service that is attended by an operator. The method
+    ///     consists of comparing the signature produced by the card user on the sales slip against the witness signature of
+    ///     the cardholder written on the back side of the card
+    /// </summary>
+    public bool SignaturePaperForContact() => _Value.IsBitSet(14);
+
+    /// <summary>
+    ///     The cardholder types his or her PIN in the terminal's PIN pad. The terminal encrypts it using a symmetric
+    ///     encryption mechanism. The IH receives this cryptogram, decrypts it in a secure module, which computes a PIN image
+    ///     control value that is compared against a witness value kept in the cardholder database, referred to as the PIN
+    ///     image stored value
+    /// </summary>
+    public bool EncipheredPinVerifiedOnlineForContact() => _Value.IsBitSet(15);
+
+    public bool CvmLimitExceeded() => _Value.IsBitSet(16);
+
+    #endregion
+
+    #region Byte 3
+
+    public bool CdCvmWithoutCdaSupported() => _Value.IsBitSet(22);
+    public bool EmvModeContactlessTransactionsNotSupported() => _Value.IsBitSet(23);
+    public bool MagstripeModeContactlessTransactionsNotSupported() => _Value.IsBitSet(24);
+    public bool StaticDataAuthentication() => _Value.IsBitSet(24);
+
+    #endregion
+
+    #region Byte 4
+
+    public bool ScaExempt() => _Value.IsBitSet(31);
+    public bool CdCvmBypassRequested() => _Value.IsBitSet(32);
 
     #endregion
 }
