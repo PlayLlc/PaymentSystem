08cdadf3dd898ada77372bda79ae3f644db835c5
play-venereal <jessevaughn.play.solutions@gmail.com>
2022-02-10 05:07:33 -0600

    asdf

---
+++ Emvco/Infrastructure/Play.Interchange/Codecs/CodecMapper.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+using Play.Codecs;
+using Play.Codecs.Integers;
+using Play.Codecs.Strings;
+using Play.Codecs.Stringsddd;
+
+namespace Play.Interchange.Codecs;
+
+public class CodecMapper
+{
+    #region Instance Values
+
+    private readonly Dictionary<string, Encoding> _CodecMap = new()
+    {
+        {nameof(Alphabetic), PlayEncoding.Alphabetic},
+        {nameof(AlphaNumeric), PlayEncoding.AlphaNumeric},
+        {nameof(AlphaNumericSpecial), PlayEncoding.AlphaNumericSpecial},
+        {nameof(AlphaSpecial), PlayEncoding.AlphaSpecial},
+
+        // TODO: Needs to be UnsignedBinaryCodec. Move logic from Play.Emv.Codecs to Play.Codecs
+        {nameof(Binary), PlayEncoding.Binary},
+        {nameof(CompressedNumeric), PlayEncoding.CompressedNumeric},
+        {nameof(Numeric), PlayEncoding.Numeric},
+        {nameof(NumericSpecial), PlayEncoding.NumericSpecial},
+        {nameof(SignedNumeric), PlayEncoding.SignedNumeric}
+    };
+
+    #endregion
+}
--- Emvco/Infrastructure/Play.Interchange/Codecs/InterchangeCodec.cs
+++ Emvco/Infrastructure/Play.Interchange/Codecs/InterchangeCodec.cs
@@ -2,39 +2,13 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 using Play.Codecs;
-using Play.Codecs.Integers;
-using Play.Codecs.Strings;
-using Play.Codecs.Stringsddd;
 using Play.Core;
 using Play.Interchange.Messages.DataFields;
 
 namespace Play.Interchange.Codecs;
-
-public class CodecMapper
-{
-    #region Instance Values
-
-    private readonly Dictionary<string, Encoding> _CodecMap = new()
-    {
-        {nameof(Alphabetic), PlayEncoding.Alphabetic},
-        {nameof(AlphaNumeric), PlayEncoding.AlphaNumeric},
-        {nameof(AlphaNumericSpecial), PlayEncoding.AlphaNumericSpecial},
-        {nameof(AlphaSpecial), PlayEncoding.AlphaSpecial},
-
-        // TODO: Needs to be UnsignedBinaryCodec. Move logic from Play.Emv.Codecs to Play.Codecs
-        {nameof(Binary), PlayEncoding.Binary},
-        {nameof(CompressedNumeric), PlayEncoding.CompressedNumeric},
-        {nameof(Numeric), PlayEncoding.Numeric},
-        {nameof(NumericSpecial), PlayEncoding.NumericSpecial},
-        {nameof(SignedNumeric), PlayEncoding.SignedNumeric}
-    };
-
-    #endregion
-}
 
 public record DataFieldMetadata(int? MinLength, int? MaxLength);
 
@@ -64,7 +38,7 @@ internal class InterchangeCodec
 
     public void GetBytes(List<byte> buffer, int offset, DataField dataField)
     {
-        var codec = _CodecMap[dataField.GetDataFieldId()];
+        (DataFieldMetadata, PlayEncoding) codec = _CodecMap[dataField.GetDataFieldId()];
 
         // Logic for max & min length validation?
 
