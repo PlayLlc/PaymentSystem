a9f9eb0b882254b4b965b315a725dc5d7c1592df
play-venereal <jessevaughn.play.solutions@gmail.com>
2022-02-11 21:04:26 -0600

    asdf

--- Emvco/Emv/Contactless/Terminal/Play.Emv.Terminal.Common.Services/CardholderVerificationMethods/CardholderVerificationMethodService.cs
+++ Emvco/Emv/Contactless/Terminal/Play.Emv.Terminal.Common.Services/CardholderVerificationMethods/CardholderVerificationMethodService.cs
@@ -1,4 +1,15 @@
 ﻿namespace Play.Emv.Terminal.Common.Services.CardholderVerificationMethods;
 
 public class CardholderVerificationMethodService
-{ }
+{
+    private readonly IVerifyCardholderPinOffline _OfflinePinAuthentication;
+    private readonly IVerifyCardholderPinOnline _OnlinePinAuthentication;
+    private readonly IVerifyCardholderSignature _CardholderSignatureVerification;
+
+    public CardholderVerificationMethodService(IVerifyCardholderPinOffline offlinePinAuthentication, IVerifyCardholderPinOnline onlinePinAuthentication, IVerifyCardholderSignature cardholderSignatureVerification)
+    {
+        _OfflinePinAuthentication = offlinePinAuthentication;
+        _OnlinePinAuthentication = onlinePinAuthentication;
+        _CardholderSignatureVerification = cardholderSignatureVerification;
+    }
+}
