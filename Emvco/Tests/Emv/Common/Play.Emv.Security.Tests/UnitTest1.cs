16d06ed3a865b4b6569b47173a15bb42f575aff6
play-venereal <jessevaughn.play.solutions@gmail.com>
2022-02-13 19:58:35 -0600

    asdf

---
+++ Emvco/Tests/Emv/Common/Play.Emv.Security.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Play.Emv.Security.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Emvco/Tests/Emv/Contactless/Play.Emv.Pcd.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Play.Emv.Pcd.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
