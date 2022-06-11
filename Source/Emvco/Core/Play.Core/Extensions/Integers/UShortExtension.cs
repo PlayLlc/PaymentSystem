fb29428f8d1fb56eabb8a0836f8190a4c910fdb0
Jesse Vaughn <69282137+JVenereal@users.noreply.github.com>
2022-06-11 12:51:28 -0500

    Ushort unit tests completed

--- Source/Emvco/Core/Play.Core/Extensions/Integers/UShortExtension.cs
+++ Source/Emvco/Core/Play.Core/Extensions/Integers/UShortExtension.cs
@@ -8,25 +8,12 @@ public static class UShortExtension
 {
     #region Instance Members
 
-    public static bool AreAnyBitsSet(this ushort value, ushort bitsToCompare) => (value & bitsToCompare) != 0;
-    public static ushort ClearBits(this in ushort input, ushort bitsToClear) => (ushort) (input & ~bitsToClear);
-    public static string GetBinaryString(this ushort value) => Convert.ToString(value, 2);
-    public static byte GetMaskedValue(this in ushort value, in ushort bitsToMask) => (byte) (value & ~bitsToMask);
-
-    /// <summary>
-    ///     This method gets the number of bits that are set to 1 in the integer
-    /// </summary>
-    public static int GetSetBitCount(this ushort value)
+    public static byte GetNumberOfDigits(this in ushort value)
     {
-        int result = 0;
+        if (value == 0)
+            return 1;
 
-        for (byte i = 0; i < Specs.Integer.UInt16.BitCount; i++)
-        {
-            if (value.IsBitSet((byte) (1 << i)))
-                result++;
-        }
-
-        return result;
+        return (byte) Math.Floor(Math.Log10(value) + 1);
     }
 
     public static byte GetMostSignificantBit(this in ushort value)
@@ -39,50 +26,38 @@ public static class UShortExtension
         return (byte) (bitLog + 1);
     }
 
+    public static byte GetMaskedValue(this in ushort value, in ushort bitsToMask) => (byte) (value & ~bitsToMask);
+
+    /// <summary>
+    ///     Determines if any of the bits provided are set in the value to be compared
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="bitsToCompare"></param>
+    /// <returns></returns>
+    public static bool AreAnyBitsSet(this ushort value, ushort bitsToCompare) => (value & bitsToCompare) != 0;
+
+    /// <summary>
+    ///     This method gets the number of bits that are set to 1 in the integer
+    /// </summary>
+    public static int GetSetBitCount(this ushort value)
+    {
+        int result = 0;
+
+        for (byte i = 0; i < Specs.Integer.UInt16.BitCount; i++)
+        {
+            if (value.IsBitSet((byte) i))
+                result++;
+        }
+
+        return result;
+    }
+
     public static byte GetMostSignificantByte(this in ushort value) =>
         (byte) (value.GetMostSignificantBit().TryGetRemainder(8, out byte resultWithoutRemainder) == 0 ? resultWithoutRemainder : resultWithoutRemainder + 1);
 
-    public static byte GetNumberOfDigits(this in ushort value)
-    {
-        if (value == 0)
-            return 1;
-
-        return (byte) Math.Floor(Math.Log10(value) + 1);
-    }
-
-    public static bool HasValue(this ushort value, ushort valueToCheck) => (value & valueToCheck) != 0;
     public static bool IsBitSet(this ushort value, byte bitPosition) => (value & (1 << bitPosition)) != 0;
     public static bool IsBitSet(this ushort value, Bits bit) => (value & BitLookup.GetBit(bit)) != 0;
     public static bool IsEven(this ushort value) => (value % 2) == 0;
-
-    public static byte LeftPaddedUnsetBitCount(this in ushort value)
-    {
-        const byte maxUshortBit = 16;
-
-        return (byte) (maxUshortBit - value.GetMostSignificantBit());
-    }
-
-    /// <summary>
-    ///     This will return the number of unset bits there are before the first set bit, starting with the least significant
-    ///     bit.
-    ///     For example, the following value:
-    ///     01011000
-    ///     would return 3.
-    /// </summary>
-    /// <param name="value"></param>
-    /// <returns></returns>
-    public static byte RightPaddedUnsetBitCount(this ushort value)
-    {
-        const byte numberOfBitsInUlong = 16;
-
-        for (byte i = 0; i < numberOfBitsInUlong; i++)
-        {
-            if ((value & (ulong) (1 << i)) != 0)
-                return i;
-        }
-
-        return 0;
-    }
 
     /// <exception cref="ArgumentOutOfRangeException"></exception>
     public static ushort SetBit(this in ushort input, byte bitPosition)
@@ -90,39 +65,7 @@ public static class UShortExtension
         if (bitPosition > Specs.Integer.UInt32.BitCount)
             throw new ArgumentOutOfRangeException(nameof(bitPosition));
 
-        return (ushort) (input | (bitPosition - 8));
-    }
-
-    /// <summary>
-    ///     Returns a value that has each bit after the most significant bit in <see cref="value" /> filled
-    /// </summary>
-    /// <param name="value"></param>
-    public static ushort GetMaskAfterMostSignificantBit(this ushort value) => (ushort) ~value.GetMaskToMostSignificantBit();
-
-    /// <summary>
-    ///     Returns a value that has each bit filled to the most significant bit in <see cref="value" />
-    /// </summary>
-    /// <param name="value"></param>
-    public static ushort GetMaskToMostSignificantBit(this ushort value)
-    {
-        byte mostSignificantBit = value.GetMostSignificantBit();
-
-        byte offset = mostSignificantBit;
-        ushort result = 0;
-
-        for (; offset < Specs.Integer.UInt8.BitCount; offset -= Specs.Integer.UInt8.BitCount)
-        {
-            result <<= 8;
-            result |= byte.MaxValue;
-        }
-
-        for (; offset > 0; offset--)
-        {
-            result <<= 1;
-            result |= 0x1;
-        }
-
-        return result;
+        return (ushort) (input | (1 << (bitPosition - 1)));
     }
 
     #endregion
--- Source/Emvco/Tests/Core/Play.Core.Tests/Tests/Extensions/Integers/UshortTests.cs
+++ Source/Emvco/Tests/Core/Play.Core.Tests/Tests/Extensions/Integers/UshortTests.cs
@@ -9,10 +9,10 @@ namespace Play.Core.Tests.Tests.Extensions.Integers;
 
 public class UshortTests : TestBase
 {
-    #region Instance Members
+    #region GetNumberOfDigits
 
     [Fact]
-    public void Ushort_GetNumberOfDigits5_ReturnsExpectedResult()
+    public void Ushort_GetNumberOfDigits_Returns5()
     {
         ushort testData = 12345;
         int expected = Specs.Integer.UInt16.MaxDigits;
@@ -21,7 +21,7 @@ public class UshortTests : TestBase
     }
 
     [Fact]
-    public void Ushort_GetNumberOfDigits3_ReturnsExpectedResult()
+    public void Ushort_GetNumberOfDigits_Returns3()
     {
         ushort testData = 123;
         int expected = 3;
@@ -30,7 +30,7 @@ public class UshortTests : TestBase
     }
 
     [Fact]
-    public void Ushort_GetNumberOfDigits0_ReturnsExpectedResult()
+    public void Ushort_GetNumberOfDigits_Returns0()
     {
         ushort testData = 0;
         int expected = 1;
@@ -38,8 +38,12 @@ public class UshortTests : TestBase
         Assertion(() => Assert.Equal(expected, actual), Build.Equals.Message(expected, actual));
     }
 
+    #endregion
+
+    #region GetMostSignificantBit
+
     [Fact]
-    public void Ushort_GetMostSignificantBit0_ReturnsExpectedResult()
+    public void Ushort_GetMostSignificantBit_Returns0()
     {
         ushort testData = 0;
         int expected = 0;
@@ -48,7 +52,7 @@ public class UshortTests : TestBase
     }
 
     [Fact]
-    public void Ushort_GetMostSignificantBit14_ReturnsExpectedResult()
+    public void Ushort_GetMostSignificantBit_Returns14()
     {
         ushort testData = 12345;
         int expected = 14;
@@ -57,7 +61,7 @@ public class UshortTests : TestBase
     }
 
     [Fact]
-    public void Ushort_GetMostSignificantBit11_3_ReturnsExpectedResult()
+    public void Ushort_GetMostSignificantBit_Returns11()
     {
         ushort testData = 0b0000010110110110;
         int expected = 11;
@@ -66,7 +70,7 @@ public class UshortTests : TestBase
     }
 
     [Fact]
-    public void Ushort_GetMostSignificantBit16_ReturnsExpectedResult()
+    public void Ushort_GetMostSignificantBit_Returns16()
     {
         ushort testData = ushort.MaxValue;
         int expected = Specs.Integer.UInt16.BitCount;
@@ -76,11 +80,248 @@ public class UshortTests : TestBase
 
     [Theory]
     [MemberData(nameof(IntFixture.MostSignificantBit.ForUShort), 50, MemberType = typeof(IntFixture.MostSignificantBit))]
-    public void RandomByteArray_InvokesConcatArrays_CreatesValueCopyWithCorrectLength(int actual, ushort testData)
+    public void Ushort_RandomGetMostSignificantBit_ReturnsExpectedResult(int actual, ushort testData)
     {
         int expected = testData.GetMostSignificantBit();
 
         Assertion(() => Assert.Equal(expected, actual), Build.Equals.Message(expected, actual));
+    }
+
+    #endregion
+
+    #region GetMaskedValue
+
+    [Fact]
+    public void Ushort_GetMaskedValue_Returns0b101000()
+    {
+        ushort testData = 0b101010;
+        ushort mask = 0b000010;
+        ushort expected = 0b101000;
+
+        Assertion(() => Assert.Equal(testData.GetMaskedValue(mask), expected));
+    }
+
+    [Fact]
+    public void Ushort_GetMaskedValue_Returns0b1()
+    {
+        ushort testData = 0b11111111;
+        ushort mask = 0b11111110;
+        ushort expected = 0b0;
+
+        Assertion(() => Assert.Equal(testData.GetMaskedValue(mask), expected));
+    }
+
+    [Fact]
+    public void Ushort_GetMaskedValue_Returns0b0()
+    {
+        ushort testData = 0b11111110;
+        ushort mask = 0b11111110;
+        ushort expected = 0b1;
+
+        Assertion(() => Assert.Equal(testData.GetMaskedValue(mask), expected));
+    }
+
+    #endregion
+
+    #region AreAnyBitsSet
+
+    [Fact]
+    public void Ushort_AreAnyBitsSet1_ReturnsTrue()
+    {
+        ushort testData = 0b101010;
+        ushort bitsToCompare = 0b100000;
+
+        Assertion(() => Assert.True(testData.AreAnyBitsSet(bitsToCompare)));
+    }
+
+    [Fact]
+    public void Ushort_AreAnyBitsSet3_ReturnsTrue()
+    {
+        ushort testData = 0b101010;
+        ushort bitsToCompare = 0b101010;
+
+        Assertion(() => Assert.True(testData.AreAnyBitsSet(bitsToCompare)));
+    }
+
+    [Fact]
+    public void Ushort_AreAnyBitsSet0_ReturnsFalse()
+    {
+        ushort testData = 0b101010;
+        ushort bitsToCompare = 0b010100;
+
+        Assertion(() => Assert.False(testData.AreAnyBitsSet(bitsToCompare)));
+    }
+
+    #endregion
+
+    #region GetSetBitCount
+
+    [Fact]
+    public void Ushort_GetSetBitCount3_ReturnsExpectedResult()
+    {
+        ushort testData = 0b101010;
+        ushort expected = 3;
+
+        Assertion(() => Assert.Equal(expected, testData.GetSetBitCount()));
+    }
+
+    [Fact]
+    public void Ushort_GetSetBitCount16_ReturnsExpectedResult()
+    {
+        ushort testData = ushort.MaxValue;
+        ushort expected = 16;
+
+        Assertion(() => Assert.Equal(expected, testData.GetSetBitCount()));
+    }
+
+    [Fact]
+    public void Ushort_GetSetBitCount0_ReturnsExpectedResult()
+    {
+        ushort testData = ushort.MinValue;
+        ushort expected = 0;
+
+        Assertion(() => Assert.Equal(expected, testData.GetSetBitCount()));
+    }
+
+    #endregion
+
+    #region GetMostSignificantByte
+
+    [Fact]
+    public void Ushort_GetMostSignificantByte_Returns0()
+    {
+        ushort testData = 0;
+        int expected = 0;
+        int actual = testData.GetMostSignificantByte();
+        Assertion(() => Assert.Equal(expected, actual), Build.Equals.Message(expected, actual));
+    }
+
+    [Fact]
+    public void Ushort_GetMostSignificantByte_Returns1()
+    {
+        ushort testData = 0xFF;
+        int expected = 1;
+        int actual = testData.GetMostSignificantByte();
+        Assertion(() => Assert.Equal(expected, actual), Build.Equals.Message(expected, actual));
+    }
+
+    [Fact]
+    public void Ushort_GetMostSignificantByte1_Returns2()
+    {
+        ushort testData = 0xFFFF;
+        int expected = 2;
+        int actual = testData.GetMostSignificantByte();
+        Assertion(() => Assert.Equal(expected, actual), Build.Equals.Message(expected, actual));
+    }
+
+    #endregion
+
+    #region IsBitSet
+
+    [Fact]
+    public void Ushort_IsBitSet2_ReturnsTrue()
+    {
+        ushort testData = 0b11111110;
+        byte bitPosition = 2;
+        Assertion(() => Assert.True(testData.IsBitSet(bitPosition)));
+    }
+
+    [Fact]
+    public void Ushort_IsBitSet1_ReturnsFalse()
+    {
+        ushort testData = 0b11111110;
+        byte bitPosition = 1;
+        Assertion(() => Assert.False(testData.IsBitSet(bitPosition)));
+    }
+
+    [Fact]
+    public void Ushort_IsBitSetBitsEight_ReturnsTrue()
+    {
+        ushort testData = ushort.MaxValue;
+        Bits bitPosition = Bits.Eight;
+        Assertion(() => Assert.True(testData.IsBitSet(bitPosition)));
+    }
+
+    [Fact]
+    public void Ushort_IsBitSetBitsTwo_ReturnsTrue()
+    {
+        ushort testData = 0b11111110;
+        Bits bitPosition = Bits.Two;
+        Assertion(() => Assert.True(testData.IsBitSet(bitPosition)));
+    }
+
+    [Fact]
+    public void Ushort_IsBitSetBitsOne_ReturnsFalse()
+    {
+        ushort testData = 0b11111110;
+        Bits bitPosition = Bits.One;
+        Assertion(() => Assert.False(testData.IsBitSet(bitPosition)));
+    }
+
+    [Fact]
+    public void Ushort_IsBitSet16_ReturnsTrue()
+    {
+        ushort testData = ushort.MaxValue;
+        byte bitPosition = 16;
+        Assertion(() => Assert.True(testData.IsBitSet(bitPosition)));
+    }
+
+    #endregion
+
+    #region IsEven
+
+    [Fact]
+    public void Ushort_IsEven_ReturnsTrue()
+    {
+        ushort testData = 8;
+        Assertion(() => Assert.True(testData.IsEven()));
+    }
+
+    [Fact]
+    public void Ushort_IsEven_ReturnsFalse()
+    {
+        ushort testData = 7;
+        Assertion(() => Assert.False(testData.IsEven()));
+    }
+
+    #endregion
+
+    #region SetBit
+
+    [Fact]
+    public void Ushort_SetBit_Returns0b1000()
+    {
+        ushort testData = 0;
+        byte bitToSet = 4;
+        ushort expected = 0b1000;
+        Assertion(() => Assert.Equal(expected, testData.SetBit(bitToSet)));
+    }
+
+    [Fact]
+    public void Ushort_SetBit_Returns0b10000()
+    {
+        ushort testData = 0;
+        byte bitToSet = 5;
+        ushort expected = 0b10000;
+        Assertion(() => Assert.Equal(expected, testData.SetBit(bitToSet)));
+    }
+
+    [Fact]
+    public void Ushort_SetBit_Returns0b00000()
+    {
+        ushort testData = 0;
+        byte bitToSet = 0;
+        ushort expected = 0;
+        Assertion(() => Assert.Equal(expected, testData.SetBit(bitToSet)));
+    }
+
+    [Fact]
+    public void Ushort_SetBit_Returns0b11111()
+    {
+        ushort testData = 0b11111;
+        byte bitToSet = 5;
+        ushort expected = 0b11111;
+        Assertion(() => Assert.Equal(expected, testData.SetBit(bitToSet)));
     }
 
     #endregion
