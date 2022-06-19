2b251b44110478bf88dfe470be44014aff0e2286
Jesse Vaughn <69282137+JVenereal@users.noreply.github.com>
2022-06-19 14:48:33 -0500

    TestEnumObject test double created and EnumObject tests completed

---
+++ Source/Emvco/Tests/Core/Play.Core.Tests/Data/TestDoubles/TestEnumObject.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Play.Core.Tests.Data.TestDoubles;
+
+public sealed record TestEnumObject : EnumObject<byte>
+{
+    #region Static Metadata
+
+    public static readonly TestEnumObject Empty = new();
+    private static readonly ImmutableSortedDictionary<byte, TestEnumObject> _ValueObjectMap;
+    public static readonly TestEnumObject First;
+    public static readonly TestEnumObject Second;
+
+    #endregion
+
+    #region Constructor
+
+    public TestEnumObject()
+    { }
+
+    /// <exception cref="TypeInitializationException"></exception>
+    static TestEnumObject()
+    {
+        const byte first = 1;
+        const byte second = 2;
+        First = new TestEnumObject(first);
+        Second = new TestEnumObject(second);
+
+        _ValueObjectMap = new Dictionary<byte, TestEnumObject> {{second, Second}, {first, First}}.ToImmutableSortedDictionary(a => a.Key, b => b.Value);
+    }
+
+    private TestEnumObject(byte value) : base(value)
+    { }
+
+    #endregion
+
+    #region Instance Members
+
+    public override TestEnumObject[] GetAll() => _ValueObjectMap.Values.ToArray();
+
+    public override bool TryGet(byte value, out EnumObject<byte>? result)
+    {
+        if (_ValueObjectMap.TryGetValue(value, out TestEnumObject? enumResult))
+        {
+            result = enumResult;
+
+            return true;
+        }
+
+        result = null;
+
+        return false;
+    }
+
+    public static bool TryGet(byte value, out TestEnumObject? result) => _ValueObjectMap.TryGetValue(value, out result);
+
+    #endregion
+
+    #region Equality
+
+    public bool Equals(TestEnumObject? other) => other is not null && (_Value == other._Value);
+
+    public bool Equals(TestEnumObject? x, TestEnumObject? y)
+    {
+        if (x is null)
+            return y is null;
+
+        if (y is null)
+            return false;
+
+        return x.Equals(y);
+    }
+
+    public int GetHashCode(TestEnumObject other) => other.GetHashCode();
+    public override int GetHashCode() => unchecked(_Value.GetHashCode() * 31771);
+
+    #endregion
+}
---
+++ Source/Emvco/Tests/Core/Play.Core.Tests/Tests/Customs/EnumObjectTests.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Play.Core.Extensions;
+using Play.Core.Tests.Data.TestDoubles;
+using Play.Testing.BaseTestClasses;
+
+using Xunit;
+
+namespace Play.Core.Tests.Tests.Customs;
+
+public class EnumObjectTests : TestBase
+{
+    #region Instance Members
+
+    [Fact]
+    public void ValidRawEnumValue_EqualityOnCorrectObject_ReturnsTrue()
+    {
+        byte testData = 1;
+
+        Assertion(() => Assert.True(testData == TestEnumObject.First));
+    }
+
+    [Fact]
+    public void ValidRawEnumValue_EqualityOnIncorrectObject_ReturnsTrue()
+    {
+        byte testData = 2;
+
+        Assertion(() => Assert.False(testData == TestEnumObject.First));
+    }
+
+    #endregion
+
+    #region GetAll
+
+    [Fact]
+    public void DefaultEnumObject_GetAll_ReturnsExpectedValue()
+    {
+        TestEnumObject testData = TestEnumObject.Empty;
+        TestEnumObject[] expected = new[] {TestEnumObject.First, TestEnumObject.Second};
+
+        TestEnumObject[] actual = testData.GetAll();
+
+        Assertion(() => Assert.Equal(expected, actual));
+    }
+
+    [Fact]
+    public void DefaultEnumObject_GetAll_IsNotNull()
+    {
+        TestEnumObject testData = TestEnumObject.Empty;
+        TestEnumObject[] expected = new[] {TestEnumObject.First, TestEnumObject.Second};
+
+        TestEnumObject[] actual = testData.GetAll();
+
+        Assertion(() => Assert.NotNull(expected));
+    }
+
+    #endregion
+
+    #region TryGet
+
+    [Fact]
+    public void ValidRawEnumValue_InstanceTryGet_ReturnsTrue()
+    {
+        byte testData = 1;
+
+        bool expected = true;
+        bool actual = TestEnumObject.Empty.TryGet(testData, out EnumObject<byte>? result);
+
+        Assertion(() => Assert.Equal(expected, actual));
+    }
+
+    [Fact]
+    public void ValidRawEnumValue_InstanceTryGet_ReturnsExpectedResult()
+    {
+        byte testData = 1;
+
+        EnumObject<byte> expected = TestEnumObject.First;
+        TestEnumObject.Empty.TryGet(testData, out EnumObject<byte>? actual);
+
+        Assertion(() => Assert.Equal(expected, actual));
+    }
+
+    [Fact]
+    public void ValidRawEnumValue_StaticTryGet_ReturnsTrue()
+    {
+        byte testData = 1;
+
+        bool expected = true;
+        bool actual = TestEnumObject.TryGet(testData, out TestEnumObject? result);
+
+        Assertion(() => Assert.Equal(expected, actual));
+    }
+
+    [Fact]
+    public void InvalidRawEnumValue_StaticTryGet_ReturnsFalse()
+    {
+        byte testData = 3;
+
+        bool actual = TestEnumObject.TryGet(testData, out TestEnumObject? result);
+
+        Assertion(() => Assert.False(actual));
+    }
+
+    [Fact]
+    public void ValidRawEnumValue_StaticTryGet_ReturnsExpectedValue()
+    {
+        byte testData = 1;
+        TestEnumObject expected = TestEnumObject.First;
+
+        TestEnumObject.TryGet(testData, out TestEnumObject? actual);
+
+        Assertion(() => Assert.Equal(expected, actual));
+    }
+
+    [Fact]
+    public void InvalidRawEnumValue_StaticTryGet_ReturnsExpectedValue()
+    {
+        byte testData = 3;
+        TestEnumObject? expected = null;
+
+        TestEnumObject.TryGet(testData, out TestEnumObject? actual);
+
+        Assertion(() => Assert.Equal(expected, actual));
+    }
+
+    #endregion
+}
