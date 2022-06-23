24cd2dddba242c7d8af8e616db0299f710ccf5a4
doroian <107423986+doroian@users.noreply.github.com>
2022-06-23 18:47:55 +0300

    Done (#18)
    Co-authored-by: Jesse Vaughn <69282137+JVenereal@users.noreply.github.com>

---
+++ Source/Emvco/Tests/Core/Play.Core.Tests/Tests/Extensions/ReadOnlySpan/ReadonlySpanCharExtensionTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+using Play.Core.Extensions;
+using Play.Testing.BaseTestClasses;
+
+using Xunit;
+
+namespace Play.Core.Tests.Tests.Extensions.ReadOnlySpan;
+
+public class ReadonlySpanCharExtensionTests : TestBase
+{
+    #region IsValueEqual
+
+    [Fact]
+    public void ReadOnlySpanChar_IsValueEqual_ReturnsExpectedResult()
+    {
+        Assertion(() =>
+        {
+            ReadOnlySpan<char> expected = stackalloc char[] { 'a', 'A', '1', '[' };
+            ReadOnlySpan<char> actual = expected;
+
+            Assert.True(actual.IsValueEqual(expected));
+        });
+    }
+
+    [Fact]
+    public void ReadOnlySpanChar_IsValueEqual_ExpectNotEqualResult()
+    {
+        Assertion(() =>
+        {
+            ReadOnlySpan<char> expected = stackalloc char[] { 'a', 'A', '1', '[' };
+            ReadOnlySpan<char> actual = stackalloc char[] { 'a', 'A', '[' };
+
+            Assert.False(actual.IsValueEqual(expected));
+        });
+    }
+
+    #endregion
+
+    #region CopyValue
+
+    [Fact]
+    public void ReadOnlySpanChar_InvokesCopyValue_CorrectlyCopiesValue()
+    {
+        ReadOnlySpan<char> testData = stackalloc char[] { 'a', 'A', '1', '[' };
+
+        char[] expected = testData.ToArray();
+        char[] actual = testData.CopyValue();
+
+        Assertion(() =>
+        {
+            Assert.NotSame(expected, actual);
+            Assert.Equal(expected, actual);
+        }, Build.Equals.Message(expected, actual));
+    }
+
+    [Fact]
+    public void ReadOnlySpanChar_InvokesCopyValue_CreatesValueCopyWithCorrectLength()
+    {
+        ReadOnlySpan<char> testData = stackalloc char[] { 'a', 'A', '1', '[' };
+
+        char[] expected = testData.ToArray();
+        char[] actual = testData.CopyValue();
+
+        Assertion(() =>
+        {
+            Assert.NotSame(expected, actual);
+            Assert.Equal(expected.Length, actual.Length);
+        }, Build.Equals.Message(expected, actual));
+    }
+
+    #endregion
+}
+
