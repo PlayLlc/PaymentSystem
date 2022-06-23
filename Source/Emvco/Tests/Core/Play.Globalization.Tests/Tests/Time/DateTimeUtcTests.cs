aa9908dc0e67da1e3219ae4ad7510b2a81e505db
doroian <doroian@paywithplay.com>
2022-06-23 15:33:37 +0300

    Adjust file structure and namespace declaration

--- Source/Emvco/Tests/Core/Play.Globalization.Tests/Tests/Time/DateTimeUtcTests.cs
+++ Source/Emvco/Tests/Core/Play.Globalization.Tests/Tests/Time/DateTimeUtcTests.cs
@@ -6,99 +6,99 @@ using Play.Testing.BaseTestClasses;
 
 using Xunit;
 
-namespace Play.Globalization.Tests.Tests.Time
+namespace Play.Globalization.Tests.Tests.Time;
+
+public class DateTimeUtcTests : TestBase
 {
-    public class DateTimeUtcTests : TestBase
+    [Fact]
+    public void DateTimeUtc_InitializeWithCorrectDateTimeKind_DateTimeUtcInitialized()
     {
-        [Fact]
-        public void DateTimeUtc_InitializeWithCorrectDateTimeKind_DateTimeUtcInitialized()
+        DateTime input = new DateTime(2019, 06, 11, 0, 0, 0, DateTimeKind.Local);
+
+        Assertion(() =>
         {
-            DateTime input = new DateTime(2019, 06, 11, 0, 0, 0, DateTimeKind.Local);
-
-            Assertion(() =>
-            {
-                Assert.Throws<PlayInternalException>(() =>
-                {
-                    DateTimeUtc test = new DateTimeUtc(input);
-                });
-            });
-        }
-
-        [Fact]
-        public void DateTimeUtc_InitializeWithCorrectDateTimeKind_DateTimeUtcInitiliazed()
-        {
-            DateTime input = new DateTime(2019, 06, 11, 0, 0, 0, DateTimeKind.Utc);
-
-            Assertion(() =>
+            Assert.Throws<PlayInternalException>(() =>
             {
                 DateTimeUtc test = new DateTimeUtc(input);
-                Assert.NotNull(test);
-                Assert.Equal(2019, test.Year);
             });
-        }
+        });
+    }
 
-        [Fact]
-        public void DateTimeUtc_InstantiateUsingLongValue_DateTimeUtcInstantiated()
+    [Fact]
+    public void DateTimeUtc_InitializeWithCorrectDateTimeKind_DateTimeUtcInitiliazed()
+    {
+        DateTime input = new DateTime(2019, 06, 11, 0, 0, 0, DateTimeKind.Utc);
+
+        Assertion(() =>
         {
-            long input = 20190615;
-            DateTimeUtc testData = new DateTimeUtc(input);
+            DateTimeUtc test = new DateTimeUtc(input);
+            Assert.NotNull(test);
+            Assert.Equal(2019, test.Year);
+        });
+    }
 
-            Assertion(() => Assert.Equal(01, testData.Year));
-        }
+    [Fact]
+    public void DateTimeUtc_InstantiateUsingLongValue_DateTimeUtcInstantiated()
+    {
+        long input = 20190615;
+        DateTimeUtc testData = new DateTimeUtc(input);
 
-        [Fact]
-        public void DateTimeUtc_InstiateUsingIntValue_DateTimeUtcInstantiated()
+        Assertion(() => Assert.Equal(01, testData.Year));
+    }
+
+    [Fact]
+    public void DateTimeUtc_InstiateUsingIntValue_DateTimeUtcInstantiated()
+    {
+        int input = 637915392;
+        DateTimeUtc testData = new DateTimeUtc(input);
+
+        Assertion(() => Assert.Equal(01, testData.Year));
+    }
+
+    [Fact]
+    public void DateTimeUtc_Instantiate_InstantiatedCorrectly()
+    {
+        DateTime input = new DateTime(2020, 07, 11, 11, 36, 10, DateTimeKind.Utc);
+        DateTimeUtc testData = new DateTimeUtc(input);
+
+        Assertion(() =>
         {
-            int input = 637915392;
-            DateTimeUtc testData = new DateTimeUtc(input);
+            Assert.Equal(2020, testData.Year);
+            Assert.Equal(07, testData.Month);
+            Assert.Equal(11, testData.Day);
+            Assert.Equal(11, testData.Hour);
+            Assert.Equal(36, testData.Minute);
+            Assert.Equal(10, testData.Second);
+        });
+    }
 
-            Assertion(() => Assert.Equal(01, testData.Year));
-        }
+    [Fact]
+    public void DateTimeUtc_StaticPropertyNow_CorrectValue()
+    {
+        DateTimeUtc utcNow = DateTimeUtc.Now;
 
-        [Fact]
-        public void DateTimeUtc_Instantiate_InstantiatedCorrectly()
+        Assertion(() =>
         {
-            DateTime input = new DateTime(2020, 07, 11, 11, 36, 10, DateTimeKind.Utc);
-            DateTimeUtc testData = new DateTimeUtc(input);
+            Assert.Equal(DateTime.UtcNow.Year, utcNow.Year);
+            Assert.Equal(DateTime.UtcNow.Month, utcNow.Month);
+            Assert.Equal(DateTime.UtcNow.Day, utcNow.Day);
+            Assert.Equal(0, utcNow.Hour);
+            Assert.Equal(0, utcNow.Minute);
+            Assert.Equal(0, utcNow.Second);
+        });
+    }
 
-            Assertion(() =>
-            {
-                Assert.Equal(2020, testData.Year);
-                Assert.Equal(07, testData.Month);
-                Assert.Equal(11, testData.Day);
-                Assert.Equal(11, testData.Hour);
-                Assert.Equal(36, testData.Minute);
-                Assert.Equal(10, testData.Second);
-            });
-        }
+    [Fact]
+    public void DateTimeUtc_StaticPropertyToday_CorrectValue()
+    {
+        DateTimeUtc today = DateTimeUtc.Today;
 
-        [Fact]
-        public void DateTimeUtc_StaticPropertyNow_CorrectValue()
+        Assertion(() =>
         {
-            DateTimeUtc utcNow = DateTimeUtc.Now;
-
-            Assertion(() =>
-            {
-                Assert.Equal(DateTime.UtcNow.Year, utcNow.Year);
-                Assert.Equal(DateTime.UtcNow.Month, utcNow.Month);
-                Assert.Equal(DateTime.UtcNow.Day, utcNow.Day);
-                Assert.Equal(0, utcNow.Hour);
-                Assert.Equal(0, utcNow.Minute);
-                Assert.Equal(0, utcNow.Second);
-            });
-        }
-
-        [Fact]
-        public void DateTimeUtc_StaticPropertyToday_CorrectValue()
-        {
-            DateTimeUtc today = DateTimeUtc.Today;
-
-            Assertion(() =>
-            {
-                Assert.Equal(DateTime.UtcNow.Year, today.Year);
-                Assert.Equal(DateTime.UtcNow.Month, today.Month);
-                Assert.Equal(DateTime.UtcNow.Day, today.Day);
-            });
-        }
+            Assert.Equal(DateTime.UtcNow.Year, today.Year);
+            Assert.Equal(DateTime.UtcNow.Month, today.Month);
+            Assert.Equal(DateTime.UtcNow.Day, today.Day);
+        });
     }
 }
+
