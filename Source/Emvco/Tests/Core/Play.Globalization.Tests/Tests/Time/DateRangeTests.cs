202136bbb76729972efbe968e3b6fe88a6f21c46
doroian <doroian@paywithplay.com>
2022-06-23 14:27:44 +0300

    Done tests

---
+++ Source/Emvco/Tests/Core/Play.Globalization.Tests/Tests/Time/DateRangeTests.cs
@@ -0,0 +1,90 @@
+﻿
+using System;
+
+using Play.Globalization.Time;
+using Play.Testing.BaseTestClasses;
+
+using Xunit;
+
+namespace Play.Globalization.Tests.Tests.Time;
+
+public class DateRangeTests : TestBase
+{
+
+    #region Instantiation
+
+    [Fact]
+    public void DateRange_Instantiate_ValidationErrorExceptionIsThrown()
+    {
+        ShortDate activationDate = new(DateTimeUtc.Now);
+        ShortDate expirationDate = new((uint)2006);
+
+        Assertion(() =>
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                DateRange testData = new DateRange(activationDate, expirationDate);
+            });
+        });
+    }
+
+    [Fact]
+    public void DateRange_Instantiate_CorrectInstantiation()
+    {
+        ShortDate activationDate = new((uint)2006);
+        ShortDate expirationDate = new(DateTimeUtc.Now);
+
+        DateRange testData = new DateRange(activationDate, expirationDate);
+
+        Assertion(() =>
+        {
+            Assert.NotNull(testData);
+            Assert.Equal(expirationDate, testData.GetExpirationDate());
+        });
+    }
+
+    [Fact]
+    public void DateRange_Initialize_CorrectActivationDateAndExpirationDate()
+    {
+        ShortDate activationDate = new(2206);
+        ShortDate expirationDate = new(2407);
+
+        DateRange dateRange = new DateRange(activationDate, expirationDate);
+
+        Assertion(() =>
+        {
+            Assert.Equal(activationDate, dateRange.GetActivationDate());
+            Assert.Equal(expirationDate, dateRange.GetExpirationDate());
+        });
+    }
+
+    [Fact]
+    public void DateRange_Initialize_IsActive()
+    {
+        ShortDate activationDate = new(2206);
+        ShortDate expirationDate = new(2407);
+
+        DateRange dateRange = new DateRange(activationDate, expirationDate);
+
+        Assertion(() =>
+        {
+            Assert.True(dateRange.IsActive());
+        });
+    }
+
+    [Fact]
+    public void DateRange_Initialize_IsExpired()
+    {
+        ShortDate activationDate = new(1906);
+        ShortDate expirationDate = new(2005);
+
+        DateRange dateRange = new DateRange(activationDate, expirationDate);
+
+        Assertion(() =>
+        {
+            Assert.True(dateRange.IsExpired());
+        });
+    }
+
+    #endregion
+}
