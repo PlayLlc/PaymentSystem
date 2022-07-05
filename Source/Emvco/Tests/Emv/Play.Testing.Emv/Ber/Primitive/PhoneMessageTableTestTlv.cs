8293759884310425f2ee449877a0cfeb194effd9
doroian <doroian@paywithplay.com>
2022-07-05 14:40:02 +0300

    Done tests, adjust IsIssuerIdentifierMatching to look at the 3 left most bytes as the INN is max length 3 bytes. Fixed small bug for AsDataStorageId

---
+++ Source/Emvco/Tests/Emv/Play.Testing.Emv/Ber/Primitive/PhoneMessageTableTestTlv.cs
@@ -0,0 +1,15 @@
+﻿using Play.Ber.Identifiers;
+using Play.Emv.Ber.DataElements;
+
+namespace Play.Testing.Emv.Ber.Primitive;
+
+public class PhoneMessageTableTestTlv : TestTlv
+{
+    private static readonly byte[] _DefaultContentOctets = new byte[] { };
+
+    public PhoneMessageTableTestTlv() : base(_DefaultContentOctets) { }
+
+    public PhoneMessageTableTestTlv(byte[] contentOctets) : base(contentOctets) {}
+
+    public override Tag GetTag() => PhoneMessageTable.Tag;
+}
