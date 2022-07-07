1e11430a5377fd365aa9838b314c39bf985440f7
doroian <107423986+doroian@users.noreply.github.com>
2022-07-07 21:29:59 +0300

    SEM1-286-tag-length-value-unit-tests (#39)
    * Done
    
    * Updates to TagLengthValue tests and some formatting
    
    Co-authored-by: Jesse Vaughn <69282137+JVenereal@users.noreply.github.com>

---
+++ Source/Emvco/Tests/Core/Play.Ber.Tests/ByteArrayFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Play.Ber.Tests;
+public static class ByteArrayFactory
+{
+    public static IEnumerable<byte> GetRandom(Random random, int count, int minValue, int maxValue)
+    {
+        if (minValue > maxValue)
+            throw new ArgumentOutOfRangeException(nameof(minValue));
+
+        for (int i = 0; i < count; i++)
+            yield return (byte)random.Next(minValue, maxValue);
+    }
+}
