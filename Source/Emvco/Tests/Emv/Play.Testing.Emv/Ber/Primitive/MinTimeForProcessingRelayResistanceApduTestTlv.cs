44750f785f4c5d7273c2cdb7aebbafd6f121a8da
doroian <107423986+doroian@users.noreply.github.com>
2022-07-13 06:56:18 +0300

    SEM1-290-data-elements-card-relay-resistance-protocol (#47)
    * Done tests for Card - Icc - Relay Resistance Protocol
    
    * update a couple spots
    
    Co-authored-by: Jesse Vaughn <69282137+JVenereal@users.noreply.github.com>

---
+++ Source/Emvco/Tests/Emv/Play.Testing.Emv/Ber/Primitive/MinTimeForProcessingRelayResistanceApduTestTlv.cs
@@ -0,0 +1,17 @@
+﻿using Play.Ber.Identifiers;
+using Play.Emv.Ber.DataElements;
+
+namespace Play.Testing.Emv.Ber.Primitive;
+
+public class MinTimeForProcessingRelayResistanceApduTestTlv : TestTlv
+{
+    private static readonly byte[] _DefaultContentOctets = { 9, 64 };
+
+    public MinTimeForProcessingRelayResistanceApduTestTlv() : base(_DefaultContentOctets) { }
+
+    public MinTimeForProcessingRelayResistanceApduTestTlv(byte[] contentOctets) : base(contentOctets)
+    {
+    }
+
+    public override Tag GetTag() => MinTimeForProcessingRelayResistanceApdu.Tag;
+}
