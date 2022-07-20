7505fe3746143568fd496d720cfa3ccee853529a
doroian <doroian@paywithplay.com>
2022-07-20 09:28:40 +0300

    1st draft

---
+++ Source/Emvco/Tests/Emv/Play.Testing.Emv/Ber/Primitive/CvmListTestTlv.cs
@@ -0,0 +1,17 @@
+﻿using Play.Ber.Identifiers;
+using Play.Emv.Ber.DataElements;
+
+namespace Play.Testing.Emv.Ber.Primitive;
+
+public class CvmListTestTlv : TestTlv
+{
+    private static readonly byte[] _DefaultContentOctets = { };
+
+    public CvmListTestTlv() : base(_DefaultContentOctets) { }
+
+    public CvmListTestTlv(byte[] contentOctets) : base(contentOctets)
+    {
+    }
+
+    public override Tag GetTag() => CvmList.Tag;
+}
