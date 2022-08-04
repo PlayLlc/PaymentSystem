4b3d135e66c80456367ccc81e29d174c7fe77b68
doroian <doroian@paywithplay.com>
2022-08-04 14:35:25 +0300

    1st draft : File Structure and Data organizing.

---
+++ Source/Emvco/Tests/Contactless/Play.Emv.Selection.Tests/ProtocolActivator/ProtocolActivatorFixture.cs
@@ -0,0 +1,92 @@
+﻿using AutoFixture;
+
+using Play.Emv.Ber.DataElements;
+using Play.Emv.Ber.Enums;
+using Play.Emv.Identifiers;
+using Play.Globalization;
+using Play.Globalization.Country;
+using Play.Globalization.Currency;
+using Play.Globalization.Language;
+
+namespace Play.Emv.Selection.Tests.ProtocolActivator;
+
+public static class ProtocolActivatorFixture
+{
+    public static IFixture RegisterGlobalizationProperties(this IFixture fixture)
+    {
+        NumericCountryCode numericCountryCode = new(840);
+        fixture.Register(() => numericCountryCode);
+
+        Alpha2CountryCode alpha2Code = new("US");
+        fixture.Register(() => alpha2Code);
+
+        Alpha2LanguageCode alpha2LanguageCode = new("en");
+        fixture.Register(() => alpha2LanguageCode);
+
+        NumericCurrencyCode numericCurrencyCode = new(840);
+        fixture.Register(() => numericCurrencyCode);
+
+        Alpha3CurrencyCode alpha3CurrencyCode = new("USD");
+        fixture.Register(() => alpha3CurrencyCode);
+
+        ShortKernelIdTypes kernelId = ShortKernelIdTypes.Kernel1;
+        fixture.Register(() => kernelId);
+
+        return fixture;
+    }
+
+    public static void RegisterTerminalTransactionQualifiers(this IFixture fixture, uint? ttqValue = null)
+    {
+        TerminalTransactionQualifiers ttq = ttqValue.HasValue
+            ? new TerminalTransactionQualifiers(ttqValue.Value)
+            : fixture.Create<TerminalTransactionQualifiers>();
+
+        fixture.Register(() => ttq);
+    }
+
+    public static void RegisterReaderContactlessTransactionLimit(this IFixture fixture, ulong amount)
+    {
+        ReaderContactlessTransactionLimitWhenCvmIsOnDevice readerContactlessTransactionLimit = new(amount);
+
+        fixture.Register<ReaderContactlessTransactionLimit>(() => readerContactlessTransactionLimit);
+    }
+
+    public static void RegisterReaderCvmRequiredLimit(this IFixture fixture, ulong amount)
+    {
+        ReaderCvmRequiredLimit readerCvmRequiredLimit = new(amount);
+
+        fixture.Register(() => readerCvmRequiredLimit);
+    }
+
+    public static void RegisterTerminalFloorLimit(this IFixture fixture, uint tFloorLimit)
+    {
+        TerminalFloorLimit terminalFloorLimit = new(tFloorLimit);
+
+        fixture.Register(() => terminalFloorLimit);
+    }
+
+    public static void RegisterTerminalCategoriesSupportedList(this IFixture fixture)
+    {
+        TerminalCategoriesSupportedList terminalCategoriesSupportedList = fixture.Create<TerminalCategoriesSupportedList>();
+
+        fixture.Register(() => terminalCategoriesSupportedList);
+    }
+
+    public static void RegisterCombinationCompositeKey(this IFixture fixture)
+    {
+        CombinationCompositeKey combinationCompositeKey = fixture.Create<CombinationCompositeKey>();
+        fixture.Freeze<CombinationCompositeKey>();
+    }
+
+    public static void RegisterAmmountAuthorizedNumeric(this IFixture fixture, ulong amount)
+    {
+        AmountAuthorizedNumeric amountAuthorizedNumeric = new AmountAuthorizedNumeric(amount);
+        fixture.Register(() => amountAuthorizedNumeric);
+    }
+
+    public static void RegisterCultureProfile(this IFixture fixture)
+    {
+        CultureProfile cultureProfile = fixture.Create<CultureProfile>();
+        fixture.Freeze<CultureProfile>();
+    }
+}
---
+++ Source/Emvco/Tests/Contactless/Play.Emv.Selection.Tests/ProtocolActivator/ProtocolActivatorTests.cs
@@ -0,0 +1,85 @@
+﻿using AutoFixture;
+
+using Moq;
+
+using Play.Emv.Ber.DataElements;
+using Play.Emv.Ber.Enums;
+using Play.Emv.Display.Contracts;
+using Play.Emv.Identifiers;
+using Play.Emv.Outcomes;
+using Play.Emv.Pcd.Contracts;
+using Play.Emv.Selection.Contracts;
+using Play.Testing.Emv.Contactless.AutoFixture;
+
+using Xunit;
+
+namespace Play.Emv.Selection.Tests.ProtocolActivator;
+
+public class ProtocolActivatorTests
+{
+    #region Instance Values
+
+    private readonly IFixture _Fixture;
+
+    private readonly Mock<IHandleDisplayRequests> _DisplayProcess;
+    private readonly Mock<IHandlePcdRequests> _ProximityCouplingDeviceEndpoint;
+
+    private readonly Start.ProtocolActivator _SystemUnderTest;
+
+    #endregion
+
+    #region Constructor
+
+    public ProtocolActivatorTests()
+    {
+        _Fixture = new ContactlessFixture().Create();
+        _Fixture.RegisterGlobalizationProperties();
+
+        _DisplayProcess = new Mock<IHandleDisplayRequests>(MockBehavior.Strict);
+        _ProximityCouplingDeviceEndpoint = new Mock<IHandlePcdRequests>(MockBehavior.Strict);
+
+        _SystemUnderTest = new Start.ProtocolActivator(_ProximityCouplingDeviceEndpoint.Object, _DisplayProcess.Object);
+    }
+
+    #endregion
+
+    [Fact]
+    public void OutcomeWithNoRestartNeeded_InvokingProtocolActivator_ProcessActivationIsNotARestart()
+    {
+        //Arrange
+        TransactionSessionId transactionSessionId = _Fixture.Create<TransactionSessionId>();
+        Outcome outcome = Outcome.Default;
+
+        _Fixture.RegisterTerminalTransactionQualifiers();
+        _Fixture.RegisterReaderContactlessTransactionLimit(1234);
+        _Fixture.RegisterReaderCvmRequiredLimit(1234);
+        _Fixture.RegisterTerminalFloorLimit(123);
+        _Fixture.RegisterTerminalCategoriesSupportedList();
+
+        DisplayMessageRequest expectedDisplayMessageRequest = GetExpectedReadyToReadDisplayMessage();
+        _DisplayProcess.Setup(m => m.Request(It.Is<DisplayMessageRequest>(x => x.Equals(expectedDisplayMessageRequest))));
+
+        TransactionProfile preProcessingIndicator = ProtocolActivatorFactory.CreatePreProcessingIndicator(_Fixture);
+
+        TransactionProfile[] entryPointConfigurations = new[] { preProcessingIndicator };
+
+        PreProcessingIndicators preprocessingIndicators = new(entryPointConfigurations);
+
+        CandidateList candidateList = new();
+
+        //Act
+        _SystemUnderTest.ActivateProtocol(transactionSessionId, outcome, preprocessingIndicators, candidateList);
+
+        //Assert
+        _DisplayProcess.Verify(m => m.Request(It.Is<DisplayMessageRequest>(x => x.Equals(expectedDisplayMessageRequest))), Times.Once);
+    }
+
+    private static DisplayMessageRequest GetExpectedReadyToReadDisplayMessage()
+    {
+        UserInterfaceRequestData.Builder? builder = UserInterfaceRequestData.GetBuilder();
+        builder.Set(MessageIdentifiers.PresentCard);
+        builder.Set(Statuses.ReadyToRead);
+
+        return new DisplayMessageRequest(builder.Complete());
+    }
+}
