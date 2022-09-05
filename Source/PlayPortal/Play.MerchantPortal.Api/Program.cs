69b32912b3aca78690dac040f13fcc73ec9f3283
doroian <doroian@paywithplay.com>
2022-09-05 11:30:01 +0300

    Add Serilog initial configuration and setup

--- Source/PlayPortal/Play.MerchantPortal.Api/Program.cs
+++ Source/PlayPortal/Play.MerchantPortal.Api/Program.cs
@@ -1,47 +1,75 @@
-using FluentValidation;
 using MerchantPortal.Infrastructure.Persistence;
 using MerchantPortal.WebApi.Filters;
 using MerchantPortal.WebApi.Mapping;
 using Play.MerchantPortal.Application;
-using System.Reflection;
+using Serilog;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-// Add services to the container.
+Log.Information("Starting {MerchantPortal} up", "MerchantPortal");
 
-builder.Services.AddControllers(options =>
+try
 {
-    options.Filters.Add<ApiFilterExceptionAttribute>();
-});
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    // Add services to the container.
 
-var connectionString = builder.Configuration.GetConnectionString("sql");
+    //Add Logging. (Serilog)
+    //Currently default sink is console. Will change to other provider probably.
+    builder.Host.UseSerilog((context, loggerConfiguration) =>
+    {
+        if (context.HostingEnvironment.IsDevelopment())
+            loggerConfiguration.WriteTo.Console();
 
-builder.Services.AddPersistenceServices(connectionString);
-builder.Services.AddApplicationServices();
+        loggerConfiguration.ReadFrom.Configuration(context.Configuration);
+    });
 
-builder.Services.AddAutoMapper(typeof(ProfileModelMapper));
+    builder.Services.AddControllers(options =>
+    {
+        options.Filters.Add<ApiFilterExceptionAttribute>();
+    });
 
-WebApplication app = builder.Build();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var connectionString = builder.Configuration.GetConnectionString("sql");
+
+    builder.Services.AddPersistenceServices(connectionString);
+    builder.Services.AddApplicationServices();
+
+    builder.Services.AddAutoMapper(typeof(ProfileModelMapper));
+
+    WebApplication app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+    else
+    {
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-else
+catch (Exception ex)
 {
-    app.UseHsts();
+    Log.Fatal(ex, "Unhandled startup exception");
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+finally
+{
+    Log.CloseAndFlush();
+}
