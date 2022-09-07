261c6bf48735a11ea95da5e7f94a6c239d31e055
doroian <doroian@paywithplay.com>
2022-09-07 13:23:33 +0300

    check for document exists before updating it.

--- Source/PlayPortal/Play.MerchantPortal.Application/Services/PointsOfSale/PoSConfigurationService.cs
+++ Source/PlayPortal/Play.MerchantPortal.Application/Services/PointsOfSale/PoSConfigurationService.cs
@@ -33,6 +33,11 @@ internal class PoSConfigurationService : IPoSConfigurationService
 
     public async Task UpdateCertificateConfigurationAsync(long id, CertificateAuthorityConfigurationDto certificateAuthorityConfiguration)
     {
+        bool exists = await _posRepository.ExistsAsync(id);
+
+        if (!exists)
+            throw new NotFoundException(nameof(PoSConfiguration), id);
+        
         var entity = _mapper.Map<CertificateAuthorityConfiguration>(certificateAuthorityConfiguration);
 
         await _posRepository.UpdateGivenFieldsAsync(id,
@@ -44,6 +49,11 @@ internal class PoSConfigurationService : IPoSConfigurationService
 
     public async Task AddPosCombinationConfigurationAsync(long id, CombinationDto combination)
     {
+        bool exists = await _posRepository.ExistsAsync(id);
+
+        if (!exists)
+            throw new NotFoundException(nameof(PoSConfiguration), id);
+
         var entity = _mapper.Map<Combination>(combination);
 
         await _posRepository.AddCombinationConfigurationAsync(id, entity);
@@ -51,6 +61,11 @@ internal class PoSConfigurationService : IPoSConfigurationService
 
     public async Task UpdatePosCombinationsConfigurationAsync(long id, IEnumerable<CombinationDto> combinations)
     {
+        bool exists = await _posRepository.ExistsAsync(id);
+
+        if (!exists)
+            throw new NotFoundException(nameof(PoSConfiguration), id);
+
         var collection = _mapper.Map<IEnumerable<Combination>>(combinations);
 
         await _posRepository.UpdateGivenFieldsAsync(id,
@@ -62,6 +77,11 @@ internal class PoSConfigurationService : IPoSConfigurationService
 
     public async Task UpdatePosDisplayConfigurationAsync(long id, DisplayConfigurationDto displayConfiguration)
     {
+        bool exists = await _posRepository.ExistsAsync(id);
+
+        if (!exists)
+            throw new NotFoundException(nameof(PoSConfiguration), id);
+
         var entity = _mapper.Map<DisplayConfiguration>(displayConfiguration);
 
         await _posRepository.UpdateGivenFieldsAsync(id,
@@ -73,6 +93,11 @@ internal class PoSConfigurationService : IPoSConfigurationService
 
     public async Task UpdatePosKernelConfigurationAsync(long id, KernelConfigurationDto kernelConfiguration)
     {
+        bool exists = await _posRepository.ExistsAsync(id);
+
+        if (!exists)
+            throw new NotFoundException(nameof(PoSConfiguration), id);
+
         var entity = _mapper.Map<KernelConfiguration>(kernelConfiguration);
 
         await _posRepository.UpdateGivenFieldsAsync(id,
@@ -84,6 +109,11 @@ internal class PoSConfigurationService : IPoSConfigurationService
 
     public async Task UpdatePosTerminalConfigurationAsync(long id, TerminalConfigurationDto terminalConfiguration)
     {
+        bool exists = await _posRepository.ExistsAsync(id);
+
+        if (!exists)
+            throw new NotFoundException(nameof(PoSConfiguration), id);
+
         var entity = _mapper.Map<TerminalConfiguration>(terminalConfiguration);
 
         await _posRepository.UpdateGivenFieldsAsync(id,
@@ -95,6 +125,11 @@ internal class PoSConfigurationService : IPoSConfigurationService
 
     public async Task UpdateProximityCouplingDeviceConfigurationAsync(long id, ProximityCouplingDeviceConfigurationDto proximityCouplingDeviceConfiguration)
     {
+        bool exists = await _posRepository.ExistsAsync(id);
+
+        if (!exists)
+            throw new NotFoundException(nameof(PoSConfiguration), id);
+
         var entity = _mapper.Map<ProximityCouplingDeviceConfiguration>(proximityCouplingDeviceConfiguration);
 
         await _posRepository.UpdateGivenFieldsAsync(id,
@@ -106,6 +141,11 @@ internal class PoSConfigurationService : IPoSConfigurationService
 
     public async Task AddCertificateConfigurationAsync(long id, CertificateConfigurationDto certificateConfiguration)
     {
+        bool exists = await _posRepository.ExistsAsync(id);
+
+        if (!exists)
+            throw new NotFoundException(nameof(PoSConfiguration), id);
+
         var entity = _mapper.Map<CertificateConfiguration>(certificateConfiguration);
 
         await _posRepository.AddCertificateConfigurationAsync(id, entity);
@@ -113,6 +153,11 @@ internal class PoSConfigurationService : IPoSConfigurationService
 
     public async Task UpdateCertificateAuthorityConfigurationAsync(long id, CertificateAuthorityConfigurationDto certificateAuthorityConfiguration)
     {
+        bool exists = await _posRepository.ExistsAsync(id);
+
+        if (!exists)
+            throw new NotFoundException(nameof(PoSConfiguration), id);
+
         var entity = _mapper.Map<CertificateAuthorityConfiguration>(certificateAuthorityConfiguration);
 
         await _posRepository.UpdateGivenFieldsAsync(id,
