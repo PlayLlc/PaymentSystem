e975426472c8342677eb74be2a14d219978f955b
doroian <doroian@paywithplay.com>
2022-09-22 12:44:23 +0300

    Small cleanup

--- Source/PlayPortal/Play.MerchantPortal.Client/Controllers/HomeController.cs
+++ Source/PlayPortal/Play.MerchantPortal.Client/Controllers/HomeController.cs
@@ -1,10 +1,7 @@
-﻿using IdentityModel.Client;
-using Microsoft.AspNetCore.Authentication;
+﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Play.MerchantPortal.Client.Models;
 using Play.MerchantPortal.Client.Models.Home;
-using System.Diagnostics;
 
 namespace Play.MerchantPortal.Client.Controllers
 {
@@ -28,18 +25,8 @@ namespace Play.MerchantPortal.Client.Controllers
             IndexViewModel vm = new IndexViewModel
             {
                 GivenName = User.Claims.SingleOrDefault(c => c.Type == "given_name")!.Value,
-                AccessToken = accessToken
+                AccessToken = accessToken!
             };
-
-            //TokenResponse response = await httpClient.RequestAuthorizationCodeTokenAsync(new AuthorizationCodeTokenRequest
-            //{
-            //    Address = "https://localhost:7191/connect/token",
-
-            //    ClientId = "merchantportal_client",
-            //    ClientSecret = "cb17c97c-0910-41c0-aafb-2b77a5838852",
-            //    RedirectUri = "https://localhost:7133/signin-oidc",
-            //    Code = User.Claims.FirstOrDefault(c => c.Type == IdentityModel.JwtClaimTypes.StateHash)?.Value,
-            //});
 
             return View(vm);
         }
