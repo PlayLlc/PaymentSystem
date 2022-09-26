aa5618e235cbdb5b528b3ca977e0c9aef495a2d3
Jesse Vaughn <69282137+JVenereal@users.noreply.github.com>
2022-09-25 21:52:59 -0500

    Updated naming conventions

---
+++ Source/Merchants/Infrastructure/Play.Persistence.Mongo/Helpers/UpdateFieldConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Play.Infrastructure.Persistence.Mongo;
+
+public class UpdateFieldConfig<_>
+{
+    #region Instance Values
+
+    public object? Value { get; set; }
+
+    public Expression<Func<_, object?>>? Field { get; set; }
+
+    #endregion
+}
