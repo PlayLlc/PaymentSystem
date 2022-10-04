407b91469a17d37d93f353c53990e5e7078c0c58
doroian <doroian@paywithplay.com>
2022-10-04 12:54:20 +0300

    1st draft

---
+++ Source/Emvco/Tests/Emv/Play.Emv.Ber.Tests/Templates/FileControlInformation/Adf/FileControlInformationProprietaryAdfTests.cs
@@ -0,0 +1,80 @@
+﻿using Play.Ber.DataObjects;
+using Play.Emv.Ber.Templates;
+using Play.Testing.BaseTestClasses;
+using Play.Testing.Emv.Ber.Constructed;
+
+using Xunit;
+
+namespace Play.Emv.Ber.Tests.Templates.FileControlInformation.Adf;
+
+public class FileControlInformationProprietaryAdfTests : TestBase
+{
+    #region Instance Members
+
+    /// <summary>
+    ///     BerEncoding_DeserializingTemplate_CreatesConstructedValue
+    /// </summary>
+    /// <exception cref="BerParsingException"></exception>
+    [Fact]
+    public void BerEncoding_DeserializingTemplate_CreatesConstructedValue()
+    {
+        FileControlInformationProprietaryAdfTestTlv testData = new();
+        FileControlInformationProprietaryAdf sut = FileControlInformationProprietaryAdf.Decode(testData.EncodeTagLengthValue());
+        Assert.NotNull(sut);
+    }
+
+    /// <summary>
+    ///     BerEncoding_DeserializingDTemplate_CorrectlyCreatesChildDataElements
+    /// </summary>
+    /// <exception cref="BerParsingException"></exception>
+    [Fact]
+    public void BerEncoding_DeserializingDTemplate_CorrectlyCreatesChildDataElements()
+    {
+        FileControlInformationProprietaryAdfTestTlv testData = new();
+        FileControlInformationProprietaryAdf sut = FileControlInformationProprietaryAdf.Decode(testData.EncodeTagLengthValue());
+        byte[]? expected = testData.EncodeTagLengthValue();
+        byte[] actual = sut.EncodeTagLengthValue();
+        Assertion(() => Assert.Equal(expected, actual), Build.Equals.Message(expected, actual));
+    }
+
+    /// <summary>
+    ///     Template_InvokingGetTagLengthValueByteCount_ReturnsExpectedResult
+    /// </summary>
+    /// <exception cref="BerParsingException"></exception>
+    [Fact]
+    public void Template_InvokingGetTagLengthValueByteCount_ReturnsExpectedResult()
+    {
+        FileControlInformationProprietaryAdfTestTlv testData = new();
+        FileControlInformationProprietaryAdf sut = FileControlInformationProprietaryAdf.Decode(testData.EncodeTagLengthValue());
+        Assert.True(sut.GetTagLengthValueByteCount() == testData.GetTagLengthValueByteCount());
+        Assert.NotNull(sut);
+    }
+
+    /// <summary>
+    ///     Template_InvokingGetValueByteCount_ReturnsExpectedResult
+    /// </summary>
+    /// <exception cref="BerParsingException"></exception>
+    [Fact]
+    public void Template_InvokingGetValueByteCount_ReturnsExpectedResult()
+    {
+        FileControlInformationProprietaryAdfTestTlv testData = new();
+        FileControlInformationProprietaryAdf sut = FileControlInformationProprietaryAdf.Decode(testData.EncodeTagLengthValue());
+        Assert.True(sut.GetValueByteCount() == testData.EncodeValue().Length);
+    }
+
+    /// <summary>
+    ///     Template_InvokingAsTagLengthValue_ReturnsExpectedResult
+    /// </summary>
+    /// <exception cref="BerParsingException"></exception>
+    [Fact]
+    public void Template_InvokingAsTagLengthValue_ReturnsExpectedResult()
+    {
+        TagLengthValue expected = FileControlInformationProprietaryAdfTestTlv.AsTagLengthValue();
+        FileControlInformationProprietaryAdf sut = FileControlInformationProprietaryAdf.Decode(expected.EncodeTagLengthValue());
+        TagLengthValue actual = sut.AsTagLengthValue();
+
+        Assert.Equal(expected, actual);
+    }
+
+    #endregion
+}
---
+++ Source/Emvco/Tests/Emv/Play.Testing.Emv/Ber/Constructed/FileControlInformationProprietaryAdfTestTlv.cs
@@ -0,0 +1,50 @@
+﻿using Play.Ber.DataObjects;
+using Play.Ber.Tags;
+using Play.Emv.Ber.Templates;
+using Play.Testing.Emv.Ber.Primitive;
+
+namespace Play.Testing.Emv.Ber.Constructed;
+
+public class FileControlInformationProprietaryAdfTestTlv : TestTlv
+{
+    #region Static Data
+
+    private static readonly byte[] _RawTagLengthValue =
+    {
+        0x61, 0x10, 0x4F, 0x07, 0xA0, 0x00, 0x00, 0x00, 0x03, 0x10,
+        0x10, 0x87, 0x01, 0x01, 0x9F, 0x2A, 0x01, 0x03
+    };
+
+    private static readonly byte[] _DefaultContentOctets =
+    {
+        0xBF, 0x0C, 0x07, 0xA0, 0x00, 0x00, 0x00, 0x03, 0x10, 0x10,
+        0x87, 1, 13,
+        0x50, 0x04, (byte) 't', (byte) 'e', (byte) 's', (byte) 't',
+        0x9F, 0x12, 0x04, (byte) 't', (byte) 'e', (byte) 's', (byte) 't',
+        0x9F, 0x11, 1, 22,
+        0x5F, 0x2D, 2, (byte)'e', (byte)'n'
+    };
+
+    private static readonly Tag Tag = FileControlInformationProprietaryAdf.Tag;
+
+    #endregion
+
+    #region Constructors
+
+    public FileControlInformationProprietaryAdfTestTlv() : base(_DefaultContentOctets) { }
+
+    public FileControlInformationProprietaryAdfTestTlv(byte[] contentOctets) : base(contentOctets)
+    {
+    }
+
+    #endregion
+
+    #region Instance Members
+
+    public static TagLengthValue AsTagLengthValue() => new(Tag, _DefaultContentOctets);
+    public static byte[] GetRawTagLengthValue() => _RawTagLengthValue;
+
+    public override Tag GetTag() => FileControlInformationProprietaryAdf.Tag;
+
+    #endregion
+}
