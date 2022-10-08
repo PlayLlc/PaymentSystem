96adfd79fb5504dcfeaf08e7246339e440489223
Jesse Vaughn <69282137+JVenereal@users.noreply.github.com>
2022-10-08 03:44:05 -0500

    Added logging

--- Source/Merchants/_DeleteMe/Program.cs
+++ Source/Merchants/_DeleteMe/Program.cs
@@ -3,6 +3,8 @@ using _DeleteMe.Extensions;
 using _DeleteMe.Identity.Entities;
 using _DeleteMe.Identity.Services;
 
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,6 +16,7 @@ builder.ConfigureApplicationServices();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Host.UseSerilog((ctx, lc) => lc.ReadFrom.Configuration(ctx.Configuration));
 
 var app = builder.Build();
 
