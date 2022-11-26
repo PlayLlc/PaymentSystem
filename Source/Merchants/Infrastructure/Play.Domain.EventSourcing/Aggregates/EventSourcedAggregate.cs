86579d201b668cb74009cc611de778b23767b3b2
Jesse Vaughn <69282137+JVenereal@users.noreply.github.com>
2022-11-26 03:19:26 -0600

    some half assed work on the event sourcing module

---
+++ Source/Merchants/Infrastructure/Play.Domain.EventSourcing/Aggregates/EventSourcedAggregate.cs
@@ -0,0 +1,43 @@
+﻿using Play.Domain.Aggregates;
+using Play.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Play.Domain.EventSourcing.Aggregates
+{
+    public abstract class EventSourcedAggregate<_TId> : Aggregate<_TId> where _TId : IEquatable<_TId>
+    {
+        private const long _NewAggregateVersion = -1;
+        private long _Version = _NewAggregateVersion;
+        private readonly ICollection<DomainEvent> _UncommittedEvents = new LinkedList<DomainEvent>();
+
+
+
+
+        protected abstract void Apply(dynamic @event); 
+         
+
+        protected void ApplyEvent(DomainEvent domainEvent, long version)
+        { 
+            if (!_UncommittedEvents.Any(x => x.GetEventId() == domainEvent.GetEventId()))
+            {
+                this.Apply((dynamic)domainEvent);
+                _Version = version;
+            }
+        }
+
+
+
+        protected  IEnumerable<DomainEvent> GetUncommittedEvents()
+        {
+            return _UncommittedEvents;
+        }
+        private void ClearUncommittedEvents()
+        {
+            _UncommittedEvents.Clear();
+        }
+    }
+}
