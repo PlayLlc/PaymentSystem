7d0b1efa0a058710eb9465b17cf8eed8cedc9964
Jesse Vaughn <69282137+JVenereal@users.noreply.github.com>
2022-12-02 20:11:58 -0600

    successfully sent modal

--- Source/WebPortal/TemporaryWebClient/TemporaryWebClient/wwwroot/js/contactUs.js
+++ Source/WebPortal/TemporaryWebClient/TemporaryWebClient/wwwroot/js/contactUs.js
@@ -12,9 +12,17 @@ $(document).ready(function () {
                     $("#Name").val("");
                     $("#Email").val("");
                     $("#EmailBody").val("");  
+                    // Modal
+                    $("#myModal #myModalLabel").html("Email Successfully Sent!")
+                    $("#myModal #modalBody").html("Your email has been sent to the Play team and we will respond to you shortly");
+                    $('#myModal').modal('toggle');
+                    $('#myModal').modal('show'); 
                 },
                 error: function (err) {
-                    alert(err);
+                    $("#myModalLabel").html("Email Was Unsuccessful")
+                    $("#modalBody").html("Please review your email details and ensure that everything is correct");
+                    $('#myModal').modal('toggle');
+                    $('#myModal').modal('show'); 
                 }
             })
 
